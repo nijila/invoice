a7dbfe99de108325620f36e7f4f1de508865304b
Nijila <nijilabhaskar11@gmail.com>
2020-08-23 17:35:09 +0530

    created app finance and model invoice

--- config/settings.py
+++ config/settings.py
@@ -83,6 +83,7 @@ INSTALLED_APPS = [
 
     # local
     'users.apps.UsersConfig',
+    'finance.apps.FinanceConfig',
 ]
 
 MIDDLEWARE = [
---
+++ finance/models/invoice.py
@@ -0,0 +1,27 @@
+# django
+from django.db import models
+from django.utils import timezone
+
+
+class Invoice(models.Model):
+    """Model definition for Invoice."""
+
+    
+
+    invoice_num = models.CharField('Invoice Number', max_length=150)
+    client_name = models.CharField('Client Name', max_length=150)
+    client_email = models.EmailField()
+    project_name = models.CharField('Project Name', max_length=300)
+    Amount =  models.DecimalField('Amount to be Charged',max_digits=12, decimal_places=2, default=0.00)
+    created = models.DateTimeField(default=timezone.now, editable=False)
+    
+
+    class Meta:
+        verbose_name = 'Invoice'
+        verbose_name_plural = 'Invoices'
+
+        
+
+    def __str__(self):
+        """Unicode representation of Invoice."""
+        return f'{self.invoice_num}'
